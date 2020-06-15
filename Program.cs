403986758ee201a274cca9f375dc26372c22d4c0
princend <waxdji3g4y3c04@gmail.com>
2020-06-15 11:05:40 +0800

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace myweb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Output("start");
+            CreateHostBuilder(args).Build().Run();
+            Output("end");
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    Output("before");
+                    webBuilder.UseStartup<Startup>();
+                    Output("build Webhost");
+                });
+
+
+        public static void Output(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}]{message}");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace myweb
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Program.Output("startup.configureservice -called");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, Microsoft.Extensions.Hosting.IApplicationLifetime applicationLifetime)
+        {
+            if (env.IsDevelopment())
+            {
+
+                applicationLifetime.ApplicationStarted.Register(() =>
+                {
+                    Program.Output("lifetime started");
+                });
+
+                applicationLifetime.ApplicationStopping.Register(() =>
+                {
+                    Program.Output("lifetime stopping");
+                });
+
+                applicationLifetime.ApplicationStopped.Register(() =>
+                {
+                    Thread.Sleep(5 * 1000);
+                    Program.Output("lifetime stopped");
+                });
+
+                Program.Output("configure isdevelopment");
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    Program.Output("get");
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+
+            var thread = new Thread(new ThreadStart(() =>
+            {
+                Thread.Sleep(5 * 1000);
+                Program.Output("triger stop webhost");
+                applicationLifetime.StopApplication();
+            }));
+            thread.Start();
+        }
+    }
+}
